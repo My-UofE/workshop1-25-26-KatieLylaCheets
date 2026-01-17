6a56df0350dd2d8b9c8f341514857f08348b2d19
KatieLylaCheets <klc224@exeter.ac.uk>
2026-01-17 12:09:29 +0000

    higher or lowe

--- game_functions.py
+++ game_functions.py
@@ -2,14 +2,14 @@ import random
 
 # function to be used by game_1: Guess the Number
 def pick_value(poss_values):
-    x = random.choice(poss_values)   
+    x = random.choice(poss_values)
     return x
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
     if (current_val > next_val) & (user_input == "h"):
         return True
-    elif (current_val < next_val) & (user_input == "l"):
+    if (current_val < next_val) & (user_input == "l"):
         return True
     else:
         return False
